11dc6673bfb620a9e943068d20597498377ac493
Galihpratamaa <galihpatama.0208@gmail.com>
2025-05-09 15:47:15 +0700

    fix_port_folio

--- portofolio/database/seeders/DatabaseSeeder.php
+++ portofolio/database/seeders/DatabaseSeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,16 +12,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
         
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
 
-        // Panggil seeder lainnya
+ 
         $this->call([
+            AboutSeeder::class,
             ContactSeeder::class,
+            HeroSeeder::class,
             HomeSeeder::class,
         ]);
     }
