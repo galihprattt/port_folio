830eb8819c784dd0b08ac55e70f147179913e7f4
Galihpratamaa <galihpatama.0208@gmail.com>
2025-05-09 18:26:39 +0700

    Add compiled Vite assets for deploymentt

--- portofolio/resources/views/layouts/app.blade.php
+++ portofolio/resources/views/layouts/app.blade.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title', 'Admin Panel')</title>
+
+    {{-- Vite assets --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    {{-- Optional: CDN Bootstrap (boleh tetap ada jika kamu gunakan) --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
